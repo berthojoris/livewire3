bb900c15c4d2e6d084cdd0047c949f3c986895e9
Bertho <bertho_joris@yahoo.co.id>
2023-08-29 17:33:59 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::get('/', App\Livewire\Home::class)->name('home');
 Route::get('/about', App\Livewire\About::class)->name('about');
 Route::get('/contact', App\Livewire\Contact::class)->name('contact');

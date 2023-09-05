626b142d6afb9c9397a699daeca2b7d9a6f0f733
Bertho <bertho_joris@yahoo.co.id>
2023-09-05 10:40:10 +0700

    Update

--- resources/views/layouts/new.blade.php
+++ resources/views/layouts/new.blade.php
@@ -7,13 +7,10 @@
 	<meta name="description" content="{{ env('APP_DESC') }}" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-	{{--
 	<link href="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
-		type="text/css" /> --}}
+		type="text/css" />
 	<link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-	{{--
 	<link href="{{ asset('assets/plugins/custom/prismjs/prismjs.bundle.css') }}" rel="stylesheet" type="text/css" />
-	--}}
 	<link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('assets/css/themes/layout/header/base/light.css') }}" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('assets/css/themes/layout/header/menu/light.css') }}" rel="stylesheet" type="text/css" />

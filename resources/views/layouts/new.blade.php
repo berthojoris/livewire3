497b56861295f590206c6bf827c08d8effa2fa01
Bertho <bertho_joris@yahoo.co.id>
2024-04-04 17:25:14 +0700

    Update

--- resources/views/layouts/new.blade.php
+++ resources/views/layouts/new.blade.php
@@ -13,7 +13,6 @@
 	<link href="{{ asset('assets/css/themes/layout/brand/dark.css') }}" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('assets/css/themes/layout/aside/dark.css') }}" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('template/favicon.ico') }}" rel="shortcut icon" />
-	@vite(['resources/js/app.js'])
 	@stack('style')
 	<script>
 		var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#37CA1C", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };
@@ -27,23 +26,17 @@
 		}
 	</style>
 	@livewireStyles
+	@livewireScripts
 </head>
 
 <body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
 	@include('partials.mobile_header')
 	@include('partials.root')
 	@include('partials.kt_quick_user')
-	{{-- @include('partials.kt_quick_cart')
-	@include('partials.kt_quick_panel')
-	@include('partials.kt_chat_modal')
-	@include('partials.sticky_toolbar')
-	@include('partials.kt_demo_panel') --}}
-
-	@livewireScripts
-	<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
-	{{-- <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script> --}}
-	<script src="{{ asset('template/js/libs.js') }}"></script>
-	@stack('script')
 </body>
-
+<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+<script src="{{ asset('template/js/libs.js') }}"></script>
+{{-- @vite(['resources/js/app.js']) --}}
+@stack('script')
 </html>
